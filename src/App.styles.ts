1d7b5b9ce8457eddd7873db30eca5135b9a2d835
KacperKluska <kacperkluska130@gmail.com>
2022-04-03 14:54:27 +0200

    style: added main page style

---
+++ src/App.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledPage = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  width: 100vw;
+  min-height: 100vh;
+  padding: 10rem;
+
+  background-color: gray;
+`;
