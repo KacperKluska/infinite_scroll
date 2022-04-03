e44b6b7c49c97bc3898030ce4851a05120726290
KacperKluska <kacperkluska130@gmail.com>
2022-04-03 16:32:28 +0200

    feature: moved logic to custom Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,13 @@
-import { useQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { StyledPage } from "./App.styles";
 import { EndMessage } from "./components/EndMessage/EndMessage";
 import { Loader } from "./components/Loader/Loader";
 import { NavItem } from "./components/NavItem/NavItem";
 import { Title } from "./components/Title/Title";
-import {
-  RocketLaunchData,
-  RocketLaunchResponse,
-} from "./interfaces/interfaces";
-import { GET_ROCKET_LAUNCHES } from "./rocketLaunchQuery";
-
-const DATA_LIMIT = 20;
+import { useRocketLaunches } from "./useRocketLaunches";
 
 function App() {
-  const [items, setItems] = useState<RocketLaunchData[]>([]);
-  const [hasMore, setHasMore] = useState<boolean>(true);
-
-  const { data, loading, fetchMore } = useQuery<RocketLaunchResponse>(
-    GET_ROCKET_LAUNCHES,
-    {
-      variables: {
-        offset: 0,
-        limit: DATA_LIMIT,
-      },
-    }
-  );
-
-  useEffect(() => {
-    setItems(data?.launchesPast ?? []);
-  }, [data]);
-
-  const fetchMoreData = async () => {
-    const result = await fetchMore({
-      variables: { offset: items.length || 0 },
-    });
-    if (!result.data.launchesPast.length) setHasMore(false);
-
-    // Delay for seeing a loading process
-    setTimeout(
-      () => setItems((prev) => [...prev, ...result.data.launchesPast]),
-      1000
-    );
-  };
+  const { items, hasMore, loading, fetchMoreData } = useRocketLaunches();
 
   return (
     <StyledPage>
@@ -52,14 +16,14 @@ function App() {
         <Loader />
       ) : (
         <InfiniteScroll
-          dataLength={items.length || 0}
+          dataLength={items.length}
           hasMore={hasMore}
           loader={<Loader />}
           next={fetchMoreData}
           endMessage={<EndMessage />}
         >
-          {items.map((item, index) => (
-            <NavItem key={index} item={item} />
+          {items.map((item) => (
+            <NavItem key={item.id} item={item} />
           ))}
         </InfiniteScroll>
       )}
---
+++ src/useRocketLaunches.ts
@@ -0,0 +1,43 @@
+import { useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+import {
+  RocketLaunchData,
+  RocketLaunchResponse,
+} from "./interfaces/interfaces";
+import { GET_ROCKET_LAUNCHES } from "./rocketLaunchQuery";
+
+const DATA_LIMIT = 20;
+
+export const useRocketLaunches = () => {
+  const [items, setItems] = useState<RocketLaunchData[]>([]);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+
+  const { data, loading, fetchMore } = useQuery<RocketLaunchResponse>(
+    GET_ROCKET_LAUNCHES,
+    {
+      variables: {
+        offset: 0,
+        limit: DATA_LIMIT,
+      },
+    }
+  );
+
+  useEffect(() => {
+    setItems(data?.launchesPast ?? []);
+  }, [data]);
+
+  const fetchMoreData = async () => {
+    const result = await fetchMore({
+      variables: { offset: items.length },
+    });
+    if (!result.data.launchesPast.length) setHasMore(false);
+
+    // Delay for seeing a loading process
+    setTimeout(
+      () => setItems((prev) => [...prev, ...result.data.launchesPast]),
+      1000
+    );
+  };
+
+  return { items, hasMore, loading, fetchMoreData };
+};
